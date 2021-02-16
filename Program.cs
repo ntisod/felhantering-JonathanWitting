4af42e7b8b5b0bf960d1d779283f49aa442027e6
Ripyserius <ripyserius@gmail.com>
2021-02-16 13:21:10 +0100

    Uppgift 1, 6 & 7

--- Program.cs
+++ Program.cs
@@ -23,6 +23,7 @@ namespace Felhantering
             Console.WriteLine("1. Exempel 1 - Utan felhantering med metoden Parse.");
             Console.WriteLine("2. Exempel 2 - Planera för fel med metoden TryParse.");
             Console.WriteLine("3. Exempel 3 - Felhantering med undantag.");
+            Console.WriteLine("7. Övning 7 - For-Loop med stegräkning");
 
             Console.WriteLine();
 
@@ -46,27 +47,112 @@ namespace Felhantering
                 case "3":
                     Exempel3();
                     break;
+                case "7":
+                    Övning7();
+                    break;
             }
 
             Console.ReadKey();
         }
 
+        // Övning 1
         static void Exempel1()
         {
+            #region OLD
             /*
              * Exempel 1. 
              * Beräkning av timlön.
              * Utan felhantering
              */
 
-            Console.Write("Ange din inkomst: ");
-            int inkomst = int.Parse(Console.ReadLine());
-            Console.Write("Ange antal timmar: ");
-            int timmar = int.Parse(Console.ReadLine());
+            //Console.Write("Ange din inkomst: ");
+            //int inkomst = int.Parse(Console.ReadLine());
+            //Console.Write("Ange antal timmar: ");
+            //int timmar = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Din timlön blev: " + (inkomst / timmar) + " kr/h.");
+            //Console.WriteLine("Din timlön blev: " + (inkomst / timmar) + " kr/h.");
+            #endregion OLD
+
+            #region Exempel 1 Jonathan
+
+            int inkomst = 0;
+            int timmar = 0;
+            bool exceptionFound = true;
+            while (exceptionFound)
+            {
+                try
+                {
+                    Console.Write("Ange din inkomst: ");
+                    inkomst = int.Parse(Console.ReadLine());
+                    Console.Write("Ange antal timmar: ");
+                    timmar = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Din timlön blev: " + (inkomst / timmar) + " kr/h.");
+
+                    exceptionFound = false;
+                }
+                catch (ArgumentNullException)
+                {
+                    exceptionFound = true;
+                    Console.WriteLine("ArgumentNullException");
+                    Console.WriteLine("Försök igen...");
+                }
+                catch (FormatException)
+                {
+                    exceptionFound = true;
+                    Console.WriteLine("FormatException");
+                    Console.WriteLine("Försök igen...");
+                }
+                catch (OverflowException)
+                {
+                    exceptionFound = true;
+                    Console.WriteLine("OverflowException");
+                    Console.WriteLine("Försök igen...");
+                }
+                catch (System.DivideByZeroException)
+                {
+                    exceptionFound = true;
+                    Console.WriteLine("System.DivideByZeroException");
+                    Console.WriteLine("Försök igen...");
+                }
+
+                finally
+                {
+                    if (!exceptionFound)
+                    {
+                        Console.WriteLine("Success!");
+                    }
+                }
+            }
+
+            #endregion Exempel 1 Jonathan
+
         }
 
+        // Övning 6 Metod
+        // Exempel2 GetIntInput(), Metod
+        static int GetIntInput(string inputString, bool canBeZero = true, string errorMessage="Mata in ett heltal tack!")
+        {
+            int inputResult = 0;
+            bool inputCompleted = false;
+
+            while (!inputCompleted)
+            {
+                Console.Write(inputString);
+                inputCompleted = int.TryParse(Console.ReadLine(), out inputResult);
+                if (!canBeZero && inputResult == 0) // Hanterar det möjliga exceptionet System.DivideByZeroException
+                {
+                    inputCompleted = false;
+                    Console.WriteLine("Talet kan inte vara 0!");
+                }
+                if (!inputCompleted)
+                    Console.WriteLine(errorMessage);
+            }
+
+            return inputResult;
+        }
+
+        // Övning 6
         static void Exempel2()
         {
             /*
@@ -75,32 +161,44 @@ namespace Felhantering
              * Planera för fel med metoden TryParse.
              */
 
+            #region OLD
+            //bool inmatat = false;
+            //int inkomst = 0;
+            //int timmar = 0;
 
-            bool inmatat = false;
+            //while (!inmatat)
+            //{
+            //    Console.Write("Ange din inkomst: ");
+            //    inmatat = int.TryParse(Console.ReadLine(), out inkomst);
+            //    if (!inmatat)
+            //        Console.WriteLine("Mata in ett heltal tack!");
+            //}
+
+            ////Nollställ variabeln
+            //inmatat = false;
+            //while (!inmatat)
+            //{
+            //    Console.Write("Ange antal timmar: ");
+            //    inmatat = int.TryParse(Console.ReadLine(), out timmar);
+            //    if (!inmatat)
+            //        Console.WriteLine("Mata in ett heltal tack!");
+            //}
+
+            //Console.WriteLine("Din timlön blev: " + (inkomst / timmar) + " kr/h.");
+            #endregion OLD
+
+            #region Exempel2 Jonathan
+
             int inkomst = 0;
             int timmar = 0;
 
-            while (!inmatat)
-            {
-                Console.Write("Ange din inkomst: ");
-                inmatat = int.TryParse(Console.ReadLine(), out inkomst);
-                if (!inmatat)
-                    Console.WriteLine("Mata in ett heltal tack!");
-            }
-
-            //Nollställ variabeln
-            inmatat = false;
-            while (!inmatat)
-            {
-                Console.Write("Ange antal timmar: ");
-                inmatat = int.TryParse(Console.ReadLine(), out timmar);
-                if (!inmatat)
-                    Console.WriteLine("Mata in ett heltal tack!");
-            }
+            inkomst = GetIntInput("Ange din inkomst: ");
+            timmar = GetIntInput("Ange antal timmar: ", false);
 
             Console.WriteLine("Din timlön blev: " + (inkomst / timmar) + " kr/h.");
-
+            #endregion Exempel2 Jonathan
         }
+
         static void Exempel3()
         {
             /*
@@ -129,6 +227,21 @@ namespace Felhantering
 
 
             Console.WriteLine("Programmet avslutades korrekt.");
+        }
+
+        // Övning 7
+        static void Övning7()
+        {
+            int start = GetIntInput("Mata in start");
+            int stop = GetIntInput("Mata in stop");
+            int steg = GetIntInput("Mata in steg", false);
+
+            Console.WriteLine("-----");
+            for (int i = start; i < stop + steg; i += steg)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("-----");
         }
     }
 }
